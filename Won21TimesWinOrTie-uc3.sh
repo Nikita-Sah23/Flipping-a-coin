c260f7907c4ea7b140ba4777ea13ec176d631ce3
nikita <nikitasah4gmail.com>
2022-08-06 21:40:00 +0530

    updated

--- Won21TimesWinOrTie-uc3.sh
+++ Won21TimesWinOrTie-uc3.sh
@@ -1,31 +1,25 @@
-echo "flipping coin 50 times"
-
+COUNTER=0
 counter_head=0
 counter_tails=0
 declare -A MyCoinArray
 
-for (( i=1; i<=50; i++ ))
+for ((i=1; i<=50; i++))
 do 
  flip=$((RANDOM%2))
 
- if [ $flip -eq 0 ] 
- then
+ if[$flip-eq 0];then
   echo "Heads"
   let counter_head=$counter_head+1
   MyCoinArray[heads]=$counter_head
 
  else
-  echo "Tails"
+  echo"Tails"
   let counter_tails=$counter_tails+1
   MyCoinArray[tails]=$counter_head
- fi
+  fi
+
 done
 
-echo "head won" ${MyCoinArray{${heads}}
-echo "tails won" ${MyCoinArray{${tails}}
-
-
-
-
-
+echo "head won"${MyCoinArray{${head}]}
+echo "tails won"${MyCoinArray{${tails}]}
 
