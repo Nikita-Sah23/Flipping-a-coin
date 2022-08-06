5dbf26cd7c5121797573ed99a1e2e1310aafabf5
nikita <nikitasah4gmail.com>
2022-08-06 20:43:45 +0530

    updated

--- MultipleTimeFlippingACoin-uc2.sh
+++ MultipleTimeFlippingACoin-uc2.sh
@@ -5,19 +5,18 @@ counter_tails=0
 declare -A MyCoinArray
 
 for ((i=1; i<=50; i++))
-
 do 
  flip=$((RANDOM%2))
 
- if[$flip -eq 0]
+ if[$flip-eq 0];then
   echo "Heads"
   let counter_head=$counter_head+1
   MyCoinArray[heads]=$counter_head
 
  else
   echo"Tails"
-  let counter_head=$counter_tails+1
-  MyCoinArray[heads]=$counter_head
+  let counter_tails=$counter_tails+1
+  MyCoinArray[tails]=$counter_head
   fi
 
 done
